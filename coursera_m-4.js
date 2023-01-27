d4dd556240059215404003f70c12efd81edcef08
Hbarts <123624553+Hbarts@users.noreply.github.com>
2023-01-27 19:54:56 +0530

    Add files via upload

---
+++ coursera_m-4.js
@@ -0,0 +1,12 @@
+(function(){
+    var a = ["jaimil","meet","bhavik","parth","sahil"];
+    for(var i=0;i<a.length;i++)
+    {
+        if(a[i].charAt(0) == "j" | a[i].charAt(0) == "J"){
+                console.log("Good Byy " + a[i]);
+        }
+        else{
+                console.log("Hello " + a[i]);
+        }
+    }
+})();
